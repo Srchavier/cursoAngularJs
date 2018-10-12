0c32c37255ed3bc220da96007db11a2fbe38c265
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-10-12 20:10:52 -0300

    aula-15

---
+++ js/aula15.controller.js
@@ -0,0 +1,29 @@
+var app = angular.module('app', ['ngResource']);
+
+app.factory('Livros', ['$resource', function ($resource) {
+    return $resource('../api/livros/:livroid', { livroid: '@livroid' });
+}])
+
+app.controller('Aula15Controller', function ($scope, Livros) {
+
+    $scope.carregaLivros = function () {
+        Livros.get(function (data) {
+            console.log(data.mensagem);
+        });
+    }
+
+    $scope.getLivro = function (livroid) {
+        Livros.get({ livroid: livroid }, function (data) {
+            console.log(data.mensagem);
+        });
+    }
+    $scope.salvarLivro = function (livroid) {
+
+        livro = {
+            'id': 40,
+            'titulo': 'DevMedia curso de angularJS'
+        }
+
+        Livros.save({}, livro);
+    }
+})
